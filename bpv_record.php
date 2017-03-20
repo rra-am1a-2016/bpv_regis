fc6d78b227084623b14668ef64f2e8f85f303bbd
Arjan de Ruijter <rra.am1b.2016@gmail.com>
2017-03-20 15:51:22 +0100

    einde les

--- bpv_record.php
+++ bpv_record.php
@@ -1,1 +1,14 @@
 Voeg een bpv bedrijf toe.
+<form class="form-signin">
+        <h2 class="form-signin-heading">registreren</h2>
+        <label for="inputStdNumber" class="sr-only">Student nummer</label>
+        <input type="number" id="inputStdNumber" class="form-control" placeholder="student nummer" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" autocomplete="new-password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" id="btn_login">Gaan!</button>
+</form>

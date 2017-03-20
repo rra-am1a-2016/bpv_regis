d8ec2980c1a724c49e4d68a3574003e70f663473
Arjan de Ruijter <rra.am1b.2016@gmail.com>
2017-03-20 15:44:31 +0100

    begin bpv regis

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <form class="form-signin">
-        <h2 class="form-signin-heading">Inloggen</h2>
+        <h2 class="form-signin-heading">registreren</h2>
         <label for="inputStdNumber" class="sr-only">Student nummer</label>
         <input type="number" id="inputStdNumber" class="form-control" placeholder="student nummer" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" autocomplete="new-password" required>
         <div class="checkbox">
           <label>
             <input type="checkbox" value="remember-me"> Remember me
--- register.php
+++ register.php
@@ -1,23 +1,8 @@
-<div class="page-header">
-        <h1>Registratie</h1>
-</div>
-
-<p class="lead">U kunt zich hier registreren</p>
-<p>
-<form class="form-horizontal">
-  <div class="form-group">
-    <label class="control-label col-sm-2" for="email">Studentnummer: </label>
-    <div class="col-sm-4">
+<form class="form-signin">
+  <h2 class="form-signin-heading">registreren</h2>
+    <label class="sr-only" for="email">Studentnummer: </label>
       <input type="number" class="form-control" id="leerlingnummer" placeholder="voor uw studentnummer in">
-    </div>
-    <div class="col-sm-6">
-    </div>
-  </div>  
-  <div class="form-group"> 
-    <div class="col-sm-offset-2 col-sm-10">
-      <button id="btn_regis" class="btn btn-default">Submit</button>
-    </div>
-  </div>
+      <button id="btn_regis" class="btn btn-lg btn-primary btn-block">Submit</button>
 </form>
 </p>
 <div class="alert alert-success" role="alert" id="alert_is_activated">

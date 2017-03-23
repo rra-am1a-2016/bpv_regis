497a5da5f2359616293509501685654b7c6bb7aa
Arjan de Ruijter <rra.am1b.2016@gmail.com>
2017-03-23 11:01:43 +0100

    ajax bpv

--- js/bpv_record.js
+++ js/bpv_record.js
@@ -1,10 +1,35 @@
 $(document).ready(function () {
    var btn = document.getElementById("btn_bpv");
 
+   var xhr = new XMLHttpRequest();
 
-   btn.onclick(function () {
-      alert("Hoi");
-   });
+   xhr.onreadystatechange = function () {
+      alert(xhr.status +  " | " + xhr.readyState);
+      if (xhr.status == 200 && xhr.readyState == 4) {
+         alert(xhr.responseText);
+      }
+   }
+
+   btn.onclick = function () {
+      var formData = {"nameCompany": document.getElementById("nameCompany").value,
+                      "phoneNumberCompany": document.getElementById("phoneNumberCompany").value,
+                      "nameContact": document.getElementById("nameContact").value,
+                      "mobilePhoneNumber": document.getElementById("mobilePhoneNumber").value,
+                      "address": document.getElementById("address").value,
+                      "addressNumber": document.getElementById("addressNumber").value,
+                      "city": document.getElementById("city").value,
+                      "postalCode": document.getElementById("postalCode").value,
+                      "urlCompany": document.getElementById("urlCompany").value,
+                      "emailContact": document.getElementById("emailContact").value,
+                      "description": document.getElementById("description").value};
+
+      var sendJSONData = JSON.stringify(formData);
+
+      xhr.open("GET", "bpv_record_data.php?jsonstr=" + sendJSONData, true);
+      xhr.send();
+
+      return false;
+   };
 
 
 

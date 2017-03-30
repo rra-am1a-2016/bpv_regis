fd8e5ff7c40821715b98cc48cd43b16d75aa2ae3
Arjan de Ruijter <rra.am1b.2016@gmail.com>
2017-03-30 09:20:09 +0200

    panel table

---
+++ bpv_show_companies.php
@@ -0,0 +1,41 @@
+<?php
+
+?>
+
+<div class="panel panel-primary">
+  <!-- Default panel contents -->
+  <div class="panel-heading">BPV bedrijven</div>
+  <div class="panel-body">
+    <p>Hieronder is een overzicht van alle potentiele BPV bedrijven die zijn
+       ingevoerd</p>
+  </div>
+
+  <!-- Table -->
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
--- userrole_links.php
+++ userrole_links.php
@@ -3,6 +3,7 @@
       switch ($_SESSION["userrole"]) {
          case "student":
             echo "<li><a href='./index.php?content=bpv_record'>BPV bedrijf toevoegen</a></li>";
+            echo "<li><a href='./index.php?content=bpv_show_companies'>Ingevoerde BPV bedrijven</a></li>";
             break;
          case "bpvco":
             echo "<li><a href='./index.php?content=bpv_std_view'>BPV student overzicht</a></li>";

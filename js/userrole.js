249badf216e6d24b3be19348dd7de3c3f9d022a3
Arjan de Ruijter <rra.am1a.2016.blok4@gmail.com>
2017-04-21 14:01:05 +0200

    binnenhalen userroles gedaan

--- js/userrole.js
+++ js/userrole.js
@@ -1,19 +1,30 @@
+// Maak een nieuw XMLHttpRequest object
 var xhr = new XMLHttpRequest();
 
+// Wacht tot de pagina geladen is...
 $(document).ready(function () {
 
+// Koppel een luisterevent toe aan het xhr-object, laat deze een anonieme functie triggeren
   xhr.onreadystatechange = function () {
+
+     // Als de readystate gelijk is aan 4 en de status 200 dan krijgen we een responsetext
      if ( xhr.readyState == 4 && xhr.status == 200) {
+        // We zetten de JSON-string responseText om naar een Javascript object.
         var data = JSON.parse(xhr.responseText);
-        //console.log(data);
+        // We halen de statustekst uit het array met shift(). Deze method haalt het eerste element uit het array
         var message = data.shift();
-        //console.log(message);
-        var fieldnames = ["id", "firstName", "infix", "lastName", "activate", "userrole"];
-        //console.log(fieldnames[0]);
+        // We geven op welke velden we willen weergeven in de tabel.
+        var fieldnames = ["id", "firstName", "infix", "lastName", "activate"];
+       // We selecteren de table body op basis van zijn id
         var tbody = document.getElementById("bpv_records_data");
+        // Als de statustekst van de pagina succes_records_found is dan...
         if ( message == "succes_records_found") {
+           // Doorlopen we het data object. Dit object bestaat weer uit andere objecten.
+           // Elk object bestaat uit een record uit de users database.
            for ( var i=0; i<data.length; i++) {
+              // Iedere for ronde is een object (een record uit de database)
               var tr = document.createElement("tr");
+              // Iedere for ronde wordt er een td gemaakt met inhoud.
               for ( var j=0; j < fieldnames.length; j++) {
                  var td = document.createElement("td");
                  var text = document.createTextNode(data[i][fieldnames[j]]);
@@ -22,13 +33,11 @@ $(document).ready(function () {
                  //console.log(data);
               }
               // Maak een array met alle optionteksten erin...
-              var optionText = ["nog geen contact", 
-                                "contact gezocht", 
-                                "contact bevestigd bedrijf", 
-                                "uitgenodigd voor gesprek", 
-                                "sollicitatiegesprek gehad", 
-                                "afgewezen", 
-                                "aangenomen"];
+              var optionText = ["student", 
+                                "docent", 
+                                "admin", 
+                                "bpvco", 
+                                "root"];
 
               // Maak aan het einde van de rij een td...
               var td = document.createElement("td");
@@ -38,18 +47,17 @@ $(document).ready(function () {
               select.setAttribute("class", "form-control");
 
               select.addEventListener("change", updateStatus);
-              select.urlCompany = data[i].urlCompany;
-
+              select.userrole = data[i].userrole;
               for (var n=0; n < optionText.length; n++) {
                   // maak een option tag..
                   var option = document.createElement("option");
 
                   // Voeg een attribuut value toe aan ieder option tag...
-                  option.setAttribute("value", n)
+                  option.setAttribute("value", optionText[n])
 
                   // Als de optionvalue uit de database matched met de optionvalue die gemaakt wordt
                   // Voeg dan het attribuut selected toe...
-                  if ( data[i].status == n ) {
+                  if ( data[i].userrole == optionText[n] ) {
                     option.setAttribute("selected", true);
                   }
 
